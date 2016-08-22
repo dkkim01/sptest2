aac0997f0f60d5c06f3e345c8b5ee45345a6aa48
User <User@UserPC63>
2016-08-22 15:31:17 +0900

    home.jsp 안녕하세요 추가 HomeController 안녕 추가

--- sp_test/src/main/java/com/test/app/HomeController.java
+++ sp_test/src/main/java/com/test/app/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("앙녕!! home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -32,6 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
 		
 		return "home";
 	}
